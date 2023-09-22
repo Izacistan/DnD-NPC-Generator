40baf62009172c136cf3878fa40f083de2ae3607
Isaac Hillaker <izacistan@protonmail.com>
2023-09-21 19:34:50 -0500

    Initial commit of the project on the main branch.

---
+++ DnD-NPC-Generator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DnD_NPC_Generator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
