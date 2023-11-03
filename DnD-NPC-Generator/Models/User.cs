344fa8318f98914aab7e043fef77d00c4f2b58d1
Isaac Hillaker <izacistan@protonmail.com>
2023-11-02 19:38:56 -0500

    Added validation to User model. Added unit tests for User model. Passing.

--- DnD-NPC-Generator/Models/User.cs
+++ DnD-NPC-Generator/Models/User.cs
@@ -7,8 +7,16 @@ namespace DnD_NPC_Generator.Models
     public class User
     {
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Name.")]
+        [StringLength(30, ErrorMessage = "Name must be 30 characters or less.")]
         public string Name { get; set; } = string.Empty;
+
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; } = string.Empty;
+
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Password must meet complexity requirements.")]
         public string Password { get; set; } = string.Empty;
         public User() { }
         public List<NPC> NPCs { get; set; } = null!;
