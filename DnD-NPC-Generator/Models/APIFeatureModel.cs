f7e411a9f138acd136091a3a3688240f74496425
Tim Ancona <tsancona@gmail.com>
2023-11-05 18:17:51 -0600

    added API spell and feature classes to store JSON data in

---
+++ DnD-NPC-Generator/Models/APIFeatureModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DnD_NPC_Generator.Models
+{
+    public class APIFeatureModel
+    {
+        public string index { get; set; }
+        public Dictionary<string, string> featureClass { get; set; }
+        public string name { get; set; }
+        public int level { get; set; }
+        public List<string> prerequisites { get; set; }
+        public List<string> description { get; set; }
+        public string url { get; set; }
+    }
+}
