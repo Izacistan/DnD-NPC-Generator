fe34ed319f0dc4c4c378696081eb2fbeda8a5493
Tim Ancona <tsancona@gmail.com>
2023-12-04 21:03:54 -0600

    configured app to auto-generate an NPC when checkbox is checked

--- DnD-NPC-Generator/Migrations/20231205030207_UpdateTestNPC.Designer.cs
+++ DnD-NPC-Generator/Migrations/20231205030207_UpdateTestNPC.Designer.cs
@@ -3,6 +3,7 @@ using DnD_NPC_Generator.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DnD_NPC_Generator.Migrations
 {
     [DbContext(typeof(NPCContext))]
-    partial class NPCContextModelSnapshot : ModelSnapshot
+    [Migration("20231205030207_UpdateTestNPC")]
+    partial class UpdateTestNPC
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
