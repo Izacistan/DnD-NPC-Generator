98fd6f937302ad240d560a70aaf23e95208b7c11
Tim Ancona <tsancona@gmail.com>
2023-12-11 18:06:25 -0600

    fixed Admin NPC list view

--- DnD-NPC-Generator/Areas/Admin/Controllers/AdminController.cs
+++ DnD-NPC-Generator/Areas/Admin/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json.Linq;
 
 
 namespace DnD_NPC_Generator.Areas.Admin.Controllers
@@ -22,12 +23,26 @@ namespace DnD_NPC_Generator.Areas.Admin.Controllers
         {
             var npcList = GetAllNpcs();
             var npcListView = new NPCListView { NPCs = npcList };
+            ViewBag.UserNameList = GetAllUserNames(npcList);
+
             return View(npcListView);
         }
 
         public List<NPC> GetAllNpcs()
         {
             return context.NPCs.Include(n => n.NPCClass).Include(n => n.NPCRace).OrderBy(c => c.NPCId).ToList();
+        }
+
+        public List<string> GetAllUserNames(List<NPC> npcList)
+        {
+            List<string> usernames = new List<string>();
+            foreach (var npc in npcList)
+            {
+                string username = context.Users.Where(u => u.Id == npc.Owner).ToList()[0].UserName;
+                usernames.Add(username);
+            }
+
+            return usernames;
         }
     }
 }
